4b30181220c01c43ba51d5f9e1511372661cbb38
Harry Ehrlich <Harry@Harry-Ehrlichs-iMac.local>
2014-03-17 22:11:37 -0400

    init

--- config/application.rb
+++ config/application.rb
@@ -1,25 +1,33 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
++--  6 lines: if defined?(Bundler)----------------------------------------------------------------------------
 
-module Di
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+module Gsnapweb
+class Application < Rails::Application
+ config.encoding = "utf-8"
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+ # Configure sensitive parameters which will be filtered from the log file.
+ config.filter_parameters += [:password]
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-  end
-end
+ # Enable escaping HTML in JSON.
+ config.active_support.escape_html_entities_in_json = true
+      config.active_record.whitelist_attributes = true
 
-config.assets.initialize_on_precompile = false
+ # Enable the asset pipeline
+ config.assets.enabled = true
+
+ # Version of your assets, change this if you want to expire all your assets
+ config.assets.version = '1.0'
+
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.smtp_settings = {
+          :address => 'mail.example.net',
+          :port    => 26,
+          :authentication => 'plain',
+          :user_name => 'xxxxxx',
+          :password => 'xxxxxx',
+ }
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.perform_deliveries = true
+config.action_mailer.default_charset = 'utf-8'
