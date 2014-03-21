4fe429d0c20236ba037b5008354148bf107e8ab9
Harry Ehrlich <Harry@Harry-Ehrlichs-iMac.local>
2014-03-21 11:07:27 -0400

    init

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Di::Application.configure do
+RubyRails4Sample::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
