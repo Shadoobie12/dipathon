e7004ed48d8b02c3fc303b2a43ff2226157402b4
Harry Ehrlich <Harry@Harry-Ehrlichs-iMac.local>
2014-03-21 12:09:39 -0400

    init

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-RubyRails4Sample::Application.initialize!
+Di::Application.initialize!
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-RubyRails4Sample::Application.configure do
+Di::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
